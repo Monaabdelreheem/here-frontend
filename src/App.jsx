aa314d5d89cfc99dbc46c59777bd14011d91947b
Mona Abdelreheem <m.abdelraheem@aol.com>
2026-03-25 22:53:41 -0500

    Set up React Router: install react-router-dom and update App.jsx for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+import SignInPage from './pages/SignInPage'
+import SignUpPage from './pages/SignUpPage'
+import MoodCheckPage from './pages/MoodCheckPage'
+import DashboardPage from './pages/DashboardPage'
+
 function App() {
   return (
     <div>
       <h1>HERE</h1>
       <p>Breathe in. You're Here.</p>
+
+      <Routes>
+        <Route path="/" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/mood" element={<MoodCheckPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
     </div>
   )
 }
